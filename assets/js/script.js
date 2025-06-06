ab74555d1834c23aaa1daa40f68d1aa9e764560b
Segura-Luca <134236522+Segura-Luca@users.noreply.github.com>
2025-06-06 20:57:11 -0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,118 @@
+'use strict';
+
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+}
+
+
+
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+}
+
+addEventOnElem(navTogglers, "click", toggleNavbar);
+
+const closeNavbar = function () {
+  navbar.classList.remove("active");
+  overlay.classList.remove("active");
+}
+
+addEventOnElem(navbarLinks, "click", closeNavbar);
+
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+const headerActive = function () {
+  if (window.scrollY > 150) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+}
+
+addEventOnElem(window, "scroll", headerActive);
+
+let lastScrolledPos = 0;
+
+const headerSticky = function () {
+  if (lastScrolledPos >= window.scrollY) {
+    header.classList.remove("header-hide");
+  } else {
+    header.classList.add("header-hide");
+  }
+
+  lastScrolledPos = window.scrollY;
+}
+
+addEventOnElem(window, "scroll", headerSticky);
+
+
+const sections = document.querySelectorAll("[data-section]");
+
+const scrollReveal = function () {
+  for (let i = 0; i < sections.length; i++) {
+    if (sections[i].getBoundingClientRect().top < window.innerHeight / 2) {
+      sections[i].classList.add("active");
+    }
+  }
+}
+
+scrollReveal();
+
+addEventOnElem(window, "scroll", scrollReveal);
+
+
+
+const addToCartButtons = document.querySelectorAll('button[aria-label="add to cart"]');
+
+const cartCountElem = document.querySelector('span[aria-label="cart count"]');
+
+
+let cartCount = 0;
+
+
+const handleAddToCart = function () {
+  cartCount++;
+  cartCountElem.textContent = cartCount;
+}
+
+
+addToCartButtons.forEach(button => {
+  button.addEventListener("click", handleAddToCart);
+});
+
+const addToWishlistButtons = document.querySelectorAll('button[aria-label="add to wishlist"]');
+const favoritesCountElem = document.querySelector('span[aria-label="favorites count"]');
+let favoritesCount = 0;
+
+const handleAddToWishlist = function () {
+  favoritesCount++;
+  favoritesCountElem.textContent = favoritesCount;
+}
+
+addToWishlistButtons.forEach(button => {
+  button.addEventListener("click", handleAddToWishlist);
+});
+
+
+
+
+
+
+
+
